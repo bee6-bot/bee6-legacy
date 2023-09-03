c7dd5c62c9206ee7af28bdceaeb6c8f0a1adbeb1
BeauTheBeau <beauthebeau@proton.me>
2023-09-03 18:48:37 +0100

    /sync-levels now requires Administrator permissions
    Signed-off-by: BeauTheBeau <beauthebeau@proton.me>

--- src/commands/levels/level.js
+++ src/commands/levels/level.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder} = require('discord.js');
+const {SlashCommandBuilder, PermissionsBitField} = require('discord.js');
 const {logMessage} = require('../../functions/utilities/loggingUtils');
 logMessage(`Hello, world! From level.js`, `INFO`);
 
