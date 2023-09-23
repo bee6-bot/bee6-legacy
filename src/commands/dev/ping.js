f91708dfca401998da4405bbb89e9245e52862bc
beauthebeau <beauthebeau@proton.me>
2023-09-23 20:19:56 +0100

    Move slash commands to a new file

--- src/commands/dev/ping.js
+++ src/commands/dev/ping.js
@@ -3,7 +3,7 @@ const {sendEmbed, EmbedType} = require('../../functions/utilities/embedUtils');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('bot-stats')
+        .setName('bee6')
         .setDescription('Replies with bot latency and other stats.'),
     async execute(interaction) {
 
@@ -23,6 +23,9 @@ module.exports = {
 
         // run git command to get branch
         const gitBranch = require('child_process').execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+        const gitRemote = require('child_process').execSync('git config --get remote.origin.url').toString().trim();
+        const gitRemoteMin = gitRemote.replace(/.*github.com\//, '').replace(/\.git$/, '');
+
 
         await sendEmbed(
             interaction,
@@ -33,6 +36,7 @@ module.exports = {
             + `**API Latency** is ${Math.round(interaction.client.ws.ping)}ms\n`
             + `**Uptime:** ${Math.floor(interaction.client.uptime / 1000 / 60 / 60)} hours, ${Math.floor(interaction.client.uptime / 1000 / 60) % 60} minutes, ${Math.floor(interaction.client.uptime / 1000) % 60} seconds\n`
             + `**Git branch:** ${gitBranch}\n`
+            + `**Git remote:** [${gitRemoteMin}](${gitRemote})\n`
             + `**AI Enabled:** ${process.env.AI_ENABLED === 'true' ? 'Yes' : 'No'}\n`
             + `**Fluent AI:** ${process.env.FLUENT_AI === 'true' ? 'Yes' : 'No'}\n`
 
