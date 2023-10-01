7c75c45189d96e91e6dd2aac1eaef39349e75633
BeauTheBeau <beauthebeau@proton.me>
2023-10-01 15:52:40 +0100

    Add guild existence and privacy validation in API routes
    Added a validation step in the getMoney, getLeaderboard, and getLevel API routes. Now, these routes first check if the guild (server) exists and if it's not private, before proceeding with the original paradigms. This was necessary to protect guild data and to prevent ambiguities in cases where a user tries to access data of non-existing or private guilds.

--- src/api/routes/getLevel.js
+++ src/api/routes/getLevel.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const {getLevelData} = require('../../functions/utilities/levelUtils');
+const guildModel = require("../../models/guildModel");
 const router = express.Router();
 
 router.get('/:serverId/:userId/level', async (req, res) => {
     const { serverId, userId } = req.params;
 
     if (!serverId || !userId) res.json({ message: 'Missing parameters!' });
-    else {
+
+    // Get the guild
+    try {
+        const guildModel = require('../../models/guildModel');
+        const guild = await guildModel.findOne({guildId: serverId});
+        if (!guild) return res.json({message: 'Guild not found'});
+        if (guild.miscSettings.private === true) return res.json({message: 'Guild not found'});
+    } catch (err) {
+        return res.json({message: 'Error getting guild!'});
+    }
+
         getLevelData(userId, serverId).then(levelData => {
             res.json({ levelData });
         }).catch(err => {
             res.json({ message: `Error getting level data.`});
         });
-    }
 });
 
 module.exports = router;
--- src/api/routes/getMoney.js
+++ src/api/routes/getMoney.js
@@ -3,16 +3,25 @@ const {getMoney} = require("../../functions/utilities/moneyUtils");
 const router = express.Router();
 
 router.get('/:serverId/:userId/money', async (req, res) => {
-    const { serverId, userId } = req.params;
+    const {serverId, userId} = req.params;
+    if (!serverId || !userId) res.json({message: 'Missing parameters!'});
 
-    if (!serverId || !userId) res.json({ message: 'Missing parameters!' });
-    else {
-        getMoney(userId, serverId).then(money => {
-            res.json({ money });
-        }).catch(err => {
-            res.json({ message: `Error getting money.`});
-        });
+    // Get the guild
+    try {
+        const guildModel = require('../../models/guildModel');
+        const guild = await guildModel.findOne({guildId: serverId});
+        if (!guild) return res.json({message: 'Guild not found'});
+        if (guild.miscSettings.private === true) return res.json({message: 'Guild not found'});
+    } catch (err) {
+        return res.json({message: 'Error getting guild!'});
     }
+
+    getMoney(userId, serverId).then(money => {
+        res.json({money});
+    }).catch(err => {
+        res.json({message: `Error getting money.`});
+    });
+
 });
 
 module.exports = router;
