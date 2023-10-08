34fb03b10ef8a958118f7c7f1da04d03d3a84149
BeauTheBeau <beauthebeau@proton.me>
2023-10-08 21:44:18 +0100

    Move getLevel and getMoney routes, add getBotInfo and getUserData routes
    Deleted getLevel.js and getMoney.js from src/api/routes and combined them into getUserData.js. Introduced getBotInfo.js to handle requests about bot-related info such as bot stats, server stats, bot features, and bot version. Updated src/index.js export to include the debug function as it's used in getBotInfo.js to fetch bot data.

--- src/index.js
+++ src/index.js
@@ -226,3 +226,5 @@ process.on('exit', exitHandler.bind(null));
 process.on('SIGINT', exitHandler.bind(null));
 process.on('SIGUSR1', exitHandler.bind(null));
 process.on('SIGUSR2', exitHandler.bind(null));
+
+module.exports = {client, debug}
