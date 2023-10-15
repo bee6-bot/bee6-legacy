6b253b76a83d470c09577371850dfe2c09b0c50e
BeauTheBeau <beauthebeau@proton.me>
2023-10-15 19:42:12 +0100

    Update guildModel with additional fields and categories
    Extended the guildSchema in guildModel by adding several new fields such as `notes`, `descriptions`, `protected` and `categories`. In addition, categories were added to each field which will be used to show the field's category in the help command. This was done to enhance the manageability and organization of the guild schema, and to provide more context about each field to the end user.

--- src/models/guildModel.js
+++ src/models/guildModel.js
@@ -1,85 +1,115 @@
 const mongoose = require('mongoose')
 
-const guildSchema = new mongoose.Schema({
+// Extra fields:
+// - protected bool: Whether the field should be protected from being edited by users, won't be shown in autocomplete
+// - notes str: Extra notes about the field, shown in autocomplete
+// - description str: The description of the field, shown in the help command
+// - categories arr: The categories of the field, shown in the help command
 
+const guildSchema = new mongoose.Schema({
     // General
-    guildID: {type: String, required: true, index: true},
-    language: {type: String, default: 'en'}, // Unused
-    disabledCommands: {type: Array, default: []},
-    miscSettings: {
-        private: {type: Boolean, default: true}, // Whether to hide the guild from the API
+    guildID: {type: String, required: true, index: true, protected: true, categories: ['general']},
+    language: {
+        type: String,
+        default: 'en',
+        enum: ['en', 'es', 'fr', 'de', 'it', 'pt', 'ru', 'ja', 'ko', 'zh-CN', 'zh-TW'],
+        categories: ['general']
     },
-    tags: { type: Array },
+    disabledCommands: {type: Array, default: [], categories: ['general']},
+    miscSettings: {
+        private: {type: Boolean, default: true, categories: ['general']}, // Whether to hide the guild from the API
+    },
+    tags: {type: Array, categories: ['general']},
 
     // Moderation
-    mutedRoleID: {type: String, default: ''},
-    modLogChannelID: {type: String, default: ''},
-    modLog: {type: Boolean, default: false},
-    continuousMessageLogging: {type: Boolean, default: false},
-    continuousMessageLoggingChannelID: {type: String, default: ''},
-    modLogIgnore: {
-        channels: {type: Array, default: []},
-        roles: {type: Array, default: []},
-        users: {type: Array, default: []},
-        events: {type: Array, default: []}
+    mutedRoleID: {type: String, default: '', categories: ['moderation']},
+    modLogChannelID: {type: String, default: '', categories: ['moderation', 'logs']},
+    modLog: {type: Boolean, default: false, categories: ['moderation', 'logs']},
+    continuousMessageLogging: {
+        type: Boolean,
+        default: false,
+        categories: ['moderation', 'continuous_message_logging', 'logs'],
+        notes: 'Whether to log messages continuously',
+        description: 'Continuous Message Logging will log every message sent in the server to the Continuous Message' +
+            'Logging Channel. This may be useful in large servers with a lot of threads/posts.'
     },
 
-    // Cooldowns
-    workCooldown: {type: Number, default: 180000}, // 3 minutes
-    robCooldown: {type: Number, default: 300000}, // 5 minute
-    gambleCooldown: {type: Number, default: 300000}, // 5 minutes
-
-    // XP
-    xp: {type: Boolean, default: true},
-    lastMee6Sync: {type: Number, default: 0},
-    mee6Syncing: {type: Boolean, default: false},
-    xpIgnore: {
-        channels: {type: Array, default: []},
-        roles: {type: Array, default: []},
-        users: {type: Array, default: []}
-    },
-    xpCooldown: {type: Number, default: 60}, // Intended for dynamic XP cooldown (based on guild activity), yet to be implemented
-
-
-    // Welcome
-    welcome: {type: Boolean, default: false},
-    welcomeChannelID: {type: String, default: ''},
-    welcomeMessage: {
+    continuousMessageLoggingChannelID: {
         type: String,
-        default: 'Hey, {{user}}! Welcome to **{{guild}}**! You are the **{{memberCount}}th** member!'
-    },
-    welcomeEmbed: {type: Boolean, default: false},
-    welcomeImage: {type: String, default: ''},
+        default: '',
+        categories: ['moderation', 'continuous_message_logging', 'logs'],
+        notes: 'The channel to log messages to',
+        modLogIgnore: {
+            channels: {type: Array, default: [] },
+            roles: {type: Array, default: [] },
+            users: {type: Array, default: [] },
+            events: {type: Array, default [] }
+        },
 
-    // Leave
-    leave: {type: Boolean, default: false},
-    leaveChannelID: {type: String, default: ''},
-    leaveMessage: {
-        type: String,
-        default: `Oh no, **{{user}}** left {{guild}! We're down to **{{memberCount}}** members!`
-    },
-    leaveEmbed: {type: Boolean, default: false},
-    leaveImage: {type: String, default: ''},
+        // Cooldowns
+        workCooldown: {type: Number, default: 180000, categories: ['cooldowns']},
+        robCooldown: {type: Number, default: 300000, categories: ['cooldowns']},
+        gambleCooldown: {type: Number, default: 300000, categories: ['cooldowns']},
 
-    // AutoMod - Kind of redundant due to Discord adding their own auto-mod system.
-    //           This functionality likely won't be implemented
-    autoMod: {type: Boolean, default: false},
-    autoModIgnore: {
-        channels: {type: Array, default: []},
-        roles: {type: Array, default: []},
-        users: {type: Array, default: []}
-    },
-    autoModTrigger: {
-        swearWords: {type: Array, default: []},
-        links: {type: Array, default: []},
-    },
+        // XP
+        xp: {type: Boolean, default: true, categories: ['xp']},
+        lastMee6Sync: {type: Number, default: 0, protected: true, categories: ['xp']},
+        mee6Syncing: {type: Boolean, default: false, protected: true, categories: ['xp']},
+        xpIgnore: {
+            channels: {type: Array, default: []},
+            roles: {type: Array, default: []},
+            users: {type: Array, default: []}
+        },
+        xpCooldown: {
+            type: Number,
+            default: 60000,
+            categories: ['xp'],
+            notes: 'The cooldown between XP gain (in milliseconds)'
+        }, // Intended for dynamic XP cooldown (based on guild activity), yet to be implemented, in ms
 
-    // Polls
-    polls: {type: Boolean, default: false},
-    pollChannelID: {type: String, default: ''},
-    pollPermissions: {type: Array, default: []},
-    pollCooldown: {type: Number, default: 0},
-})
+
+        // Welcome
+        welcome: {type: Boolean, default: false, categories: ['welcome', 'member_events']},
+        welcomeChannelID: {type: String, default: '', categories: ['welcome', 'member_events']},
+        welcomeMessage: {
+            type: String,
+            default: 'Hey, {{user}}! Welcome to **{{guild}}**! You are the **{{memberCount}}th** member!',
+            categories: ['welcome', 'member_events'],
+        },
+        welcomeEmbed: {type: Boolean, default: false, categories: ['welcome', 'member_events']},
+        welcomeImage: {type: String, default: '', categories: ['welcome', 'member_events']},
+
+        // Leave
+        leave: {type: Boolean, default: false, categories: ['leave', 'member_events']},
+        leaveChannelID: {type: String, default: '', categories: ['leave', 'member_events']},
+        leaveMessage: {
+            type: String,
+            default: `Oh no, **{{user}}** left {{guild}}! We're down to **{{memberCount}}** members!`,
+            categories: ['leave', 'member_events']
+        },
+        leaveEmbed: {type: Boolean, default: false, categories: ['leave', 'member_events']},
+        leaveImage: {type: String, default: '', categories: ['leave', 'member_events']},
+
+        // AutoMod - Kind of redundant due to Discord adding their own auto-mod system.
+        //           This functionality likely won't be implemented
+        autoMod: {type: Boolean, default: false, categories: ['auto_mod']},
+        autoModIgnore: {
+            channels: {type: Array, default: [], categories: ['auto_mod']},
+            roles: {type: Array, default: [], categories: ['auto_mod']},
+            users: {type: Array, default: [], categories: ['auto_mod']}
+        },
+        autoModTrigger: {
+            swearWords: {type: Array, default: [], categories: ['auto_mod']},
+            links: {type: Array, default: [], categories: ['auto_mod']},
+        },
+
+        // Polls
+        polls: {type: Boolean, default: false, categories: ['polls']},
+        pollChannelID: {type: String, default: '', categories: ['polls']},
+        pollPermissions: {type: Array, default: [], categories: ['polls']},
+        pollCooldown: {type: Number, default: 0, categories: ['polls']},
+    });
+
 
 guildSchema.index({guildID: 1}, {unique: true})
 module.exports = mongoose.model('Guild', guildSchema)
